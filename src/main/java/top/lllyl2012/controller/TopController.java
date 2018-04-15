958caa38527ae6c4e5b04f6ce6db44459eca40f0
mySelf <229707363@qq.com>
2018-04-15 16:50:49 +0800

    first

---
+++ src/main/java/top/lllyl2012/controller/TopController.java
@@ -0,0 +1,12 @@
+package top.lllyl2012.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TopController {
+	@RequestMapping("/index.do")
+	public String index() {
+		return "index";
+	}
+}
