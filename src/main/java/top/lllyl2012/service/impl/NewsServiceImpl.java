6c17ca7ecca648dcb46c46c776af3820244623d4
lllyl2012 <qibuqimingzi@163.com>
2018-06-10 11:07:25 +0800

    new

--- src/main/java/top/lllyl2012/service/impl/NewsServiceImpl.java
+++ src/main/java/top/lllyl2012/service/impl/NewsServiceImpl.java
@@ -1,11 +1,12 @@
 package top.lllyl2012.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import top.lllyl2012.bean.News;
 import top.lllyl2012.bean.mapper.NewsMapper;
 import top.lllyl2012.service.NewsService;
-
+@Service
 public class NewsServiceImpl implements NewsService{
 
 	@Autowired
